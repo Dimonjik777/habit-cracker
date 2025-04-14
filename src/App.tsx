82b23ecae31cf3107489d3e6f181c09a8b57fe80
George <demina.ivann@gmail.com>
2025-04-14 10:21:51 -0700

    Include welcome page in routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";
 function App() {
   return (
     <Router>
       <div className="main">
         <Routes>
           <Route path="/" element={<div>Home page</div>} />
+          <Route path="/welcome" element={<Welcome />} />
         </Routes>
       </div>
     </Router>
