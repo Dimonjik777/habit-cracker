04d4aa50b6c236d18f03e0ff0134a877f5650ba9
George <demina.ivann@gmail.com>
2025-04-14 09:25:01 -0700

    Add WelcomeHeader component

---
+++ src/components/welcome/WelcomeHeader.tsx
@@ -0,0 +1,17 @@
+import Logo from "/src/assets/logo.svg?react";
+import Button from "../Button";
+import styles from "/src/styles/modules/welcome-header.module.scss";
+export default function WelcomeHeader() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <Logo />
+        <div className={styles.logoText}>Habit Cracker</div>
+      </div>
+      <div className={styles.right}>
+        <Button type="primary" value="Sign In" />
+        <Button type="secondary" value="Sign Up" />
+      </div>
+    </div>
+  );
+}
