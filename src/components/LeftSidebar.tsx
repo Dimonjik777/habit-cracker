71c7d7dd13a11b29d6a068de30c004525c0d2a01
George <demina.ivann@gmail.com>
2025-04-16 08:12:07 -0700

    Replace divs with NavLink React Router component

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -1,11 +1,10 @@
 import Logo from "/src/assets/logo.svg?react";
 import ListIcon from "/src/assets/list.svg?react";
 import ChartIcon from "/src/assets/chart.svg?react";
-import { useLocation } from "react-router";
+import { NavLink } from "react-router";
 import styles from "/src/styles/modules/left-sidebar.module.scss";
 
 export default function LeftSidebar() {
-  const location = useLocation();
   return (
     <div className={styles.container}>
       <div className={styles.logoContainer}>
@@ -13,22 +12,24 @@ export default function LeftSidebar() {
         <h1>Habit Cracker</h1>
       </div>
       <div className={styles.linksContainer}>
-        <div
-          className={`${styles.link} ${
-            location.pathname == "/dashboard/all" ? styles.active : ""
-          }`}
+        <NavLink
+          to="/dashboard/all"
+          className={({ isActive }) =>
+            `${styles.link} ${isActive ? styles.active : ""}`
+          }
         >
           <ListIcon />
           <h4>All habits</h4>
-        </div>
-        <div
-          className={`${styles.link} ${
-            location.pathname == "/dashboard/statistics" ? styles.active : ""
-          }`}
+        </NavLink>
+        <NavLink
+          to="/dashboard/statistics"
+          className={({ isActive }) =>
+            `${styles.link} ${isActive ? styles.active : ""}`
+          }
         >
           <ChartIcon />
-          <h4>Statistics</h4>
-        </div>
+          <h4>All habits</h4>
+        </NavLink>
       </div>
       <div className={styles.signoutContainer}>
         <input type="button" value="Sign Out" />
