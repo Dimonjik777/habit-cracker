fa9b861aa553427ce417dbf48500d7e13df9e358
George <demina.ivann@gmail.com>
2025-04-16 08:32:06 -0700

    Use useTheme in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import ModalWindow from "./components/ModalWindow";
 import FormRegister from "./components/FormRegister";
 import FormLogin from "./components/FormLogin";
 import LeftSidebar from "./components/LeftSidebar";
+import { useTheme } from "./contexts/ThemeContext";
 
 function App() {
+  const { darkTheme } = useTheme();
   return (
     <Router>
-      <div className="main">
+      <div className={`main ${darkTheme ? "theme-dark" : ""}`}>
         <Routes>
           {/* Development-only route */}
           <Route
