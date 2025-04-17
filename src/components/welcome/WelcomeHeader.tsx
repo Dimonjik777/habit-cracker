e0a7f09d4594871b55dc7176d786548ff7a27dbc
Dmytro Kopaihora <47076154+Dimonjik777@users.noreply.github.com>
2025-04-17 17:06:12 +0300

    Use modalWindow in buttons welcome page

--- src/components/welcome/WelcomeHeader.tsx
+++ src/components/welcome/WelcomeHeader.tsx
@@ -1,7 +1,14 @@
 import Logo from "/src/assets/logo.svg?react";
 import Button from "../Button";
 import styles from "/src/styles/modules/welcome-header.module.scss";
+import { useModal } from "../../contexts/ModalContext";
+import FormRegister from "../FormRegister";
+import FormLogin from "../FormLogin";
+
 export default function WelcomeHeader() {
+
+  const {openModal} = useModal();
+
   return (
     <div className={styles.container}>
       <div className={styles.left}>
@@ -10,10 +17,14 @@ export default function WelcomeHeader() {
       </div>
       <div className={styles.right}>
         <div className={styles.buttonContainer}>
-          <Button type="primary" value="Sign In" action={() => {}} />
+          <Button type="primary" value="Sign In" action={() => {
+            openModal(<FormLogin />);
+          }} />
         </div>
         <div className={styles.buttonContainer}>
-          <Button type="secondary" value="Sign Up" action={() => {}} />
+          <Button type="secondary" value="Sign Up" action={() => {
+            openModal(<FormRegister />)
+          }} />
         </div>
       </div>
     </div>
