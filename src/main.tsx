8b55d7052d9ada9daf9b6fdea2db3798b476a349
Dmytro Kopaihora <47076154+Dimonjik777@users.noreply.github.com>
2025-04-23 18:01:55 +0300

    Add greeting to user

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import "./styles/globals/index.scss";
 import App from "./App.tsx";
 import { ThemeProvider } from "./contexts/ThemeContext.tsx";
 import { UserProvider } from "./contexts/UserContext.tsx";
+import { BrowserRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </ThemeProvider>
+    </Router>
   </StrictMode>
 );
