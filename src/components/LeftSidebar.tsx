b2a0316583cc5ae5d3c6ebf3e08f0e60ffbebc87
George <demina.ivann@gmail.com>
2025-04-24 10:22:35 -0700

    Apply SidebarContext for opening, showing and closing the sidebars

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -3,41 +3,45 @@ import ListIcon from "/src/assets/list.svg?react";
 import ChartIcon from "/src/assets/chart.svg?react";
 import { NavLink } from "react-router";
 import { useUser } from "../contexts/UserContext";
+import { useSidebar } from "../contexts/SidebarContext";
 import styles from "/src/styles/modules/left-sidebar.module.scss";
 import Logout from "/src/assets/logout.svg?react";
 
 export default function LeftSidebar() {
   const { logout } = useUser();
-
+  const { leftSidebarOpen, closeLeftSidebar } = useSidebar();
   return (
-    <div className={styles.container}>
-      <div className={styles.logoContainer}>
-        <Logo />
-        <h1>Habit Cracker</h1>
-      </div>
-      <div className={styles.linksContainer}>
-        <NavLink
-          to="/dashboard/all"
-          className={({ isActive }) =>
-            `${styles.link} ${isActive ? styles.active : ""}`
-          }
-        >
-          <ListIcon />
-          <h4>All habits</h4>
-        </NavLink>
-        <NavLink
-          to="/dashboard/statistics"
-          className={({ isActive }) =>
-            `${styles.link} ${isActive ? styles.active : ""}`
-          }
-        >
-          <ChartIcon />
-          <h4>Statistics</h4>
-        </NavLink>
-      </div>
-      <div className={styles.signoutContainer} onClick={() => logout()}>
-        <Logout />
-        <h4>Sign out</h4>
+    <div className={leftSidebarOpen ? styles.open : ""}>
+      <div className={styles.overflow} onClick={closeLeftSidebar}></div>
+      <div className={styles.container}>
+        <div className={styles.logoContainer}>
+          <Logo />
+          <h1>Habit Cracker</h1>
+        </div>
+        <div className={styles.linksContainer}>
+          <NavLink
+            to="/dashboard/all"
+            className={({ isActive }) =>
+              `${styles.link} ${isActive ? styles.active : ""}`
+            }
+          >
+            <ListIcon />
+            <h4>All habits</h4>
+          </NavLink>
+          <NavLink
+            to="/dashboard/statistics"
+            className={({ isActive }) =>
+              `${styles.link} ${isActive ? styles.active : ""}`
+            }
+          >
+            <ChartIcon />
+            <h4>Statistics</h4>
+          </NavLink>
+        </div>
+        <div className={styles.signoutContainer} onClick={() => logout()}>
+          <Logout />
+          <h4>Sign out</h4>
+        </div>
       </div>
     </div>
   );
--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -1,14 +1,17 @@
 import Calendar from "./Calendar";
 import HabitsPieChart from "./HabitsPieChart";
-
+import { useSidebar } from "../contexts/SidebarContext";
 import styles from "/src/styles/modules/right-sidebar.module.scss";
 
 export default function RightSidebar() {
-
+  const { rightSidebarOpen, closeRightSidebar } = useSidebar();
   return (
-    <div className={styles.container}>
-      <HabitsPieChart />
-      <Calendar />
+    <div className={rightSidebarOpen ? styles.open : ""}>
+      <div className={styles.overflow} onClick={closeRightSidebar}></div>
+      <div className={styles.container}>
+        <HabitsPieChart />
+        <Calendar />
+      </div>
     </div>
-  )
+  );
 }
