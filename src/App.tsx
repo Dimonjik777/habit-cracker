e2c9a01deffb9e2bf34ba5b607ae79ff7f76c22d
George <demina.ivann@gmail.com>
2025-04-28 07:14:12 -0700

    Add 404 routes with placeholders

--- src/App.tsx
+++ src/App.tsx
@@ -22,11 +22,29 @@ function App() {
                 path="statistics"
                 element={<div>Dashboard Statistics</div>}
               />
+              <Route
+                path="*"
+                element={
+                  <div>
+                    <h1>404</h1>
+                    <p>Nested page not found </p>
+                  </div>
+                }
+              />
             </Route>
           )}
           {user.role === "unregistered" && (
             <Route path="/welcome" element={<Welcome />} />
           )}
+          <Route
+            path="*"
+            element={
+              <div className="main">
+                <h1>404</h1>
+                <p>Page not found</p>
+              </div>
+            }
+          />
         </Routes>
         <ModalWindow />
       </div>
