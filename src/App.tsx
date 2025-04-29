28d9235d51f8c764b7993e9367c387c69efad595
George <demina.ivann@gmail.com>
2025-04-29 06:28:58 -0700

    Use NotFound component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { useUser } from "./contexts/UserContext";
 import Redirect from "./components/Redirect";
 import { ModalProvider } from "./contexts/ModalContext";
 import DashboardLayout from "./components/DashboardLayout";
+import NotFound from "./components/NotFound";
 
 function App() {
   const { darkTheme } = useTheme();
@@ -22,29 +23,13 @@ function App() {
                 path="statistics"
                 element={<div>Dashboard Statistics</div>}
               />
-              <Route
-                path="*"
-                element={
-                  <div>
-                    <h1>404</h1>
-                    <p>Nested page not found </p>
-                  </div>
-                }
-              />
+              <Route path="*" element={<NotFound />} />
             </Route>
           )}
           {user.role === "unregistered" && (
             <Route path="/welcome" element={<Welcome />} />
           )}
-          <Route
-            path="*"
-            element={
-              <div className="main">
-                <h1>404</h1>
-                <p>Page not found</p>
-              </div>
-            }
-          />
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <ModalWindow />
       </div>
