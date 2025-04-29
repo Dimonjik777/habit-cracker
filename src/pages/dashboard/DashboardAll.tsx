ddaeb79c8d6a18d2bfa8580946ca497993e1bd35
George <demina.ivann@gmail.com>
2025-04-29 11:02:49 -0700

    Create header in DashboardAll page component

--- src/pages/dashboard/DashboardAll.tsx
+++ src/pages/dashboard/DashboardAll.tsx
@@ -1,5 +1,40 @@
 import styles from "/src/styles/modules/dashboard-all.module.scss";
+import ArrowLeft from "/src/assets/arrow-left.svg?react";
+import ArrowRight from "/src/assets/arrow-right.svg?react";
+import { useState, useEffect } from "react";
 
 export default function DashboardAll() {
-  return <div className={styles.container}></div>;
+  const [weekday, setWeekday] = useState<string>("");
+  const [dateString, setDateString] = useState<string>("");
+  useEffect(() => {
+    const date = new Date();
+    const weekday = date.toLocaleDateString("en-US", { weekday: "long" });
+    setWeekday(weekday);
+    const dateString = date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    setDateString(dateString);
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.dateTitleContainer}>
+          <h2>{weekday}</h2>
+          {/* can you help me with this one: */}
+          <p>{dateString}</p>
+        </div>
+        <div className={styles.dateToggleContainer}>
+          <div className={styles.iconContainer}>
+            <ArrowLeft />
+          </div>
+          <div className={styles.iconContainer}>
+            <ArrowRight />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
