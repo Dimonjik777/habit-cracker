b1faf49dec335158ff30f9eed4fab99c6ceb243a
George <demina.ivann@gmail.com>
2025-05-01 09:08:11 -0700

    Close modal on "Cancel" click

--- src/components/habit-form/AddHabit.tsx
+++ src/components/habit-form/AddHabit.tsx
@@ -1,8 +1,9 @@
 import HabitForm from "./HabitForm";
 import FormButton from "./FormButton";
 import styles from "/src/styles/modules/habit-form.module.scss";
-
+import { useModal } from "../../contexts/ModalContext";
 export default function AddHabit() {
+  const { closeModal } = useModal();
   return (
     <HabitForm>
       <div className={styles.submitContainer}>
@@ -10,7 +11,7 @@ export default function AddHabit() {
           <FormButton type="primary" value="Add habit" action={() => {}} />
         </div>
         <div className={styles.buttonContainer}>
-          <FormButton type="secondary" value="Cancel" action={() => {}} />
+          <FormButton type="secondary" value="Cancel" action={closeModal} />
         </div>
       </div>
     </HabitForm>
