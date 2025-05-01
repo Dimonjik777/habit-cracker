592a38697d03b3746eb42c20f3ef29595469ee23
George <demina.ivann@gmail.com>
2025-05-01 07:25:50 -0700

    Create AddButton component

---
+++ src/components/AddButton.tsx
@@ -0,0 +1,9 @@
+import styles from "/src/styles/modules/add-button.module.scss";
+
+export default function AddButton() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>+</div>
+    </div>
+  );
+}
