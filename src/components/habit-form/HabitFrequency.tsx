88a8253bf5b06d980c19a5d49c5cec95384e7115
George <demina.ivann@gmail.com>
2025-05-03 07:20:08 -0700

    Create HabitFrequency component (only dropdown yet)

---
+++ src/components/habit-form/HabitFrequency.tsx
@@ -0,0 +1,77 @@
+import styles from "/src/styles/modules/habit-form.module.scss";
+import DropdownArrow from "/src/assets/dropdown-arrow.svg?react";
+import { useEffect, useState } from "react";
+
+export default function HabitFrequency({}: {}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const days = ["mon", "tue", "wen", "thu", "fri", "sat", "sun"];
+  const [selectedDays, setSelectedDays] = useState<string[]>(days);
+  const [currentValue, setCurrentValue] = useState<string>("Daily");
+  const toggleDropdown = () => {
+    setIsOpen((prev) => !prev);
+  };
+  useEffect(() => {
+    document.addEventListener("click", (e) => {
+      const target = e.target as HTMLElement;
+      if (!target.closest(`.${styles.selectContainer}`)) {
+        setIsOpen(false);
+      }
+    });
+  }, []);
+  return (
+    <div className={styles.habitFrequency}>
+      <div className={styles.selectContainer}>
+        <div className={styles.title}>Frequency:</div>
+        <div
+          className={`${styles.selectWrapper} ${isOpen ? styles.open : ""}`}
+          onClick={toggleDropdown}
+        >
+          <div className={styles.selectLabel}>
+            <div className={styles.text}>{currentValue}</div>
+            <span className={styles.arrowContainer}>
+              <DropdownArrow />
+            </span>
+          </div>
+          <div className={styles.selectOptions}>
+            <div
+              className={styles.option}
+              key={0}
+              onClick={() => {
+                setCurrentValue("Daily");
+                setSelectedDays(days);
+                setTimeout(() => {
+                  setIsOpen(false);
+                }, 0);
+              }}
+            >
+              Daily
+            </div>
+            <div
+              className={styles.option}
+              key={1}
+              onClick={() => {
+                setCurrentValue("Weekly");
+                setSelectedDays(selectedDays);
+                setTimeout(() => {
+                  setIsOpen(false);
+                }, 0);
+              }}
+            >
+              Weekly
+            </div>
+          </div>
+          {/* */}
+        </div>
+      </div>
+      {/* <div className="habit-frequency__days">
+        <div className="">Mon</div>
+        <div className="">Tue</div>
+        <div className="">Wen</div>
+        <div className="">Thu</div>
+        <div className="">Fri</div>
+        <div className="">Sat</div>
+        <div className="">Sun</div>
+      </div> */}
+    </div>
+  );
+}
