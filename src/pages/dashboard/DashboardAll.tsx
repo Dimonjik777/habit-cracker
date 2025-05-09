ff61942ffcf431d9dae9bfdc4a874fa4b4848dad
George <demina.ivann@gmail.com>
2025-05-09 08:47:37 -0700

    Always set date search param when on /dasboard/all page

--- src/pages/dashboard/DashboardAll.tsx
+++ src/pages/dashboard/DashboardAll.tsx
@@ -28,6 +28,15 @@ export default function DashboardAll() {
       })
     );
   }, [location.search]);
+
+  // in case date search param is not set, set it manually
+  useEffect(() => {
+    const currentDate = getDateParam() ?? formatDate(new Date());
+    const [day, month, year] = currentDate.split("-").map(Number);
+    const date = new Date(year, month - 1, day);
+    navigate(`?date=${formatDate(date)}`);
+  }, []);
+
   const formatDate = (date: Date): string => {
     const day = String(date.getDate()).padStart(2, "0");
     const month = String(date.getMonth() + 1).padStart(2, "0");
