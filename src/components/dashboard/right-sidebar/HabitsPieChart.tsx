01efcc5dac3c85aa69dd55d828334c3db9fab26b
George <demina.ivann@gmail.com>
2025-05-09 07:48:42 -0700

    Make habits pie chart dynamic

--- src/components/dashboard/right-sidebar/HabitsPieChart.tsx
+++ src/components/dashboard/right-sidebar/HabitsPieChart.tsx
@@ -1,35 +1,47 @@
 import { PieChart, Pie, Cell } from "recharts";
 import styles from "/src/styles/modules/dashboard/right-sidebar/habits-pie-chart.module.scss";
+import { useHabit } from "../../../contexts/HabitContext";
+import { useLocation } from "react-router-dom";
 
 export default function HabitsPieChart() {
-  const habits = [
-    { title: "Drink", isComplete: true },
-    { title: "Water", isComplete: true },
-    { title: "Learn", isComplete: false },
-  ];
+  const { habits: userHabits } = useHabit();
+  const habits = Object.values(userHabits);
+  const location = useLocation();
+  const getSearchParamDateKey = () => {
+    const searchParams = new URLSearchParams(location.search);
+    return searchParams.get("date") ?? "00-00-0000";
+  };
+  if (habits) {
+    const date = getSearchParamDateKey();
+    console.log(date);
+    const totalCompleted = habits.filter((element) => {
+      const history = element.history;
+      if (history[date]) {
+        const historyRecord = history[date];
+        return historyRecord.isCompleted;
+      }
+    }).length;
+    const totalUncompleted = habits.length - totalCompleted;
+    const completedRate = Math.floor((totalCompleted / habits.length) * 100);
 
-  const totalComplete = habits.filter((element) => element.isComplete).length;
-  const totalUncomplete = habits.length - totalComplete;
-  const completeRate = Math.floor((totalComplete / habits.length) * 100);
+    const data = [
+      { name: "Complete", value: totalCompleted },
+      { name: "Uncomplete", value: totalUncompleted },
+    ];
 
-  const data = [
-    { name: "Complete", value: totalComplete },
-    { name: "Uncomplete", value: totalUncomplete },
-  ];
-
-  const COLORS = ["#F96363", "#C7C7C7"];
-
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.complete__percent}>{`${completeRate}%`}</h1>
-      <p className={styles.complete__desc}>Current day`s progress</p>
-      <PieChart width={270} height={270}>
-        <Pie dataKey="value" data={data} innerRadius={115} outerRadius={135}>
-          {data.map((_, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index]} />
-          ))}
-        </Pie>
-      </PieChart>
-    </div>
-  );
+    const COLORS = ["#F96363", "#C7C7C7"];
+    return (
+      <div className={styles.container}>
+        <h1 className={styles.complete__percent}>{`${completedRate}%`}</h1>
+        <p className={styles.complete__desc}>Current day`s progress</p>
+        <PieChart width={270} height={270}>
+          <Pie dataKey="value" data={data} innerRadius={115} outerRadius={135}>
+            {data.map((_, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index]} />
+            ))}
+          </Pie>
+        </PieChart>
+      </div>
+    );
+  }
 }
