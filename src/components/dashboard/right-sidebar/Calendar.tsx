9c997277e23fe47ba6cae8d1a851403cf69968c0
George <demina.ivann@gmail.com>
2025-05-09 08:19:36 -0700

    Add calendar date select handler to dynamicly set the date search param

--- src/components/dashboard/right-sidebar/Calendar.tsx
+++ src/components/dashboard/right-sidebar/Calendar.tsx
@@ -1,11 +1,26 @@
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { useState } from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import dayjs from "dayjs";
+import { useNavigate } from "react-router-dom";
 
 export default function Calendar() {
+  const [selectedDate, setSelectedDate] = useState<dayjs.Dayjs | null>(dayjs());
+  const navigate = useNavigate();
+
+  const handleDateChange = (newDate: dayjs.Dayjs | null) => {
+    setSelectedDate(newDate);
+    navigate(`?date=${newDate?.format("DD-MM-YYYY")}`);
+  };
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateCalendar />
+      <DateCalendar
+        value={selectedDate}
+        onChange={handleDateChange}
+        disableFuture
+      />
     </LocalizationProvider>
   );
 }
