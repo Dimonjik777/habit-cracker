9f1a1f9d256a34cd638c1e19bca1ccc4eb649104
George <demina.ivann@gmail.com>
2025-05-10 10:27:55 -0700

    Type chosenHabit the proper way

--- src/pages/dashboard/DashboardStatistics.tsx
+++ src/pages/dashboard/DashboardStatistics.tsx
@@ -1,17 +1,25 @@
 import { useEffect, useState } from "react";
 import styles from "/src/styles/modules/dashboard/statistics/dashboard-statistics.module.scss";
 import { useHabit } from "../../contexts/HabitContext";
+import { HabitType } from "../../helpers/type-habit";
 
 export default function DashboardStatistics() {
   const { habits } = useHabit();
   const [habitsArray, setHabitsArray] = useState(Object.values(habits));
-  const [chosenHabit, setChosenHabit] = useState({ id: "" });
+  const [chosenHabit, setChosenHabit] = useState<HabitType | undefined>(
+    undefined
+  );
   useEffect(() => {
     setHabitsArray(Object.values(habits));
   }, [habits]);
   useEffect(() => {
     console.log(chosenHabit);
   }, [chosenHabit]);
+  const isActive = (habit: HabitType) => {
+    if (chosenHabit) {
+      return chosenHabit.id == habit.id ? styles.active : "";
+    }
+  };
   return (
     <div className={styles.container}>
       <div className={styles.chooseHabitContainer}>
@@ -20,9 +28,7 @@ export default function DashboardStatistics() {
           {habitsArray.map((habit) => (
             <span
               key={habit.id}
-              className={`${styles.habitCard} ${
-                chosenHabit.id == habit.id ? styles.active : ""
-              }`}
+              className={`${styles.habitCard} ${isActive(habit)}`}
               onClick={() => setChosenHabit(habit)}
             >
               {habit.title}
