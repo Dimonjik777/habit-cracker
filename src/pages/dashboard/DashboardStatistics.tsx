4d02f0aa32064239ad9cb532fd9f8f36e3a14eee
George <demina.ivann@gmail.com>
2025-05-10 07:34:22 -0700

    Create DashboardStatistics page component

---
+++ src/pages/dashboard/DashboardStatistics.tsx
@@ -0,0 +1,5 @@
+import styles from "/src/styles/modules/dashboard/statistics/dashboard-statistics.module.scss";
+
+export default function DashboardStatistics() {
+  return <div className={styles.container}></div>;
+}
