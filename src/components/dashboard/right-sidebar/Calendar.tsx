84b47a0056c8e9f3162e04cb5afc12f918e1add3
George <demina.ivann@gmail.com>
2025-05-10 07:04:19 -0700

    Update calendar's value if date param changes because of something else

--- src/components/dashboard/right-sidebar/Calendar.tsx
+++ src/components/dashboard/right-sidebar/Calendar.tsx
@@ -1,19 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
 import dayjs from "dayjs";
 import { useNavigate } from "react-router-dom";
-
+import { useLocation } from "react-router-dom";
 export default function Calendar() {
   const [selectedDate, setSelectedDate] = useState<dayjs.Dayjs | null>(dayjs());
   const navigate = useNavigate();
-
   const handleDateChange = (newDate: dayjs.Dayjs | null) => {
     setSelectedDate(newDate);
     navigate(`?date=${newDate?.format("DD-MM-YYYY")}`);
   };
-
+  const safeParseDate = (str: string) => {
+    const [dd, mm, yyyy] = str.split("-");
+    return new Date(Number(yyyy), Number(mm) - 1, Number(dd));
+  };
+  const location = useLocation();
+  useEffect(() => {
+    const dateParam = new URLSearchParams(location.search).get("date");
+    if (dateParam) {
+      const date = safeParseDate(dateParam);
+      setSelectedDate(dayjs(date));
+    }
+  }, [location.search]);
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DateCalendar
