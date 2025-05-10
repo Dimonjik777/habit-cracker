d0d3f759427e7e601fd2897a82b386503a3d499e
George <demina.ivann@gmail.com>
2025-05-10 07:35:46 -0700

    Add habit selector at statistics page

--- src/pages/dashboard/DashboardStatistics.tsx
+++ src/pages/dashboard/DashboardStatistics.tsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react";
 import styles from "/src/styles/modules/dashboard/statistics/dashboard-statistics.module.scss";
+import { useHabit } from "../../contexts/HabitContext";
 
 export default function DashboardStatistics() {
-  return <div className={styles.container}></div>;
+  const { habits } = useHabit();
+  const [habitsArray, setHabitsArray] = useState(Object.values(habits));
+  const [chosenHabit, setChosenHabit] = useState({ id: "" });
+  useEffect(() => {
+    setHabitsArray(Object.values(habits));
+  }, [habits]);
+  useEffect(() => {
+    console.log(chosenHabit);
+  }, [chosenHabit]);
+  return (
+    <div className={styles.container}>
+      <div className={styles.chooseHabitContainer}>
+        <h3>Choose a habit:</h3>
+        <div className={styles.habitCardsContainer}>
+          {habitsArray.map((habit) => (
+            <span
+              key={habit.id}
+              className={`${styles.habitCard} ${
+                chosenHabit.id == habit.id ? styles.active : ""
+              }`}
+              onClick={() => setChosenHabit(habit)}
+            >
+              {habit.title}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
