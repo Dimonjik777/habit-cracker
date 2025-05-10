173f4cbaf8c6800d49e736acbb3dab7ca821736d
George <demina.ivann@gmail.com>
2025-05-10 10:32:14 -0700

    Handle a case when there are no habits yet

--- src/pages/dashboard/DashboardStatistics.tsx
+++ src/pages/dashboard/DashboardStatistics.tsx
@@ -24,17 +24,23 @@ export default function DashboardStatistics() {
     <div className={styles.container}>
       <div className={styles.chooseHabitContainer}>
         <h3>Choose a habit:</h3>
-        <div className={styles.habitCardsContainer}>
-          {habitsArray.map((habit) => (
-            <span
-              key={habit.id}
-              className={`${styles.habitCard} ${isActive(habit)}`}
-              onClick={() => setChosenHabit(habit)}
-            >
-              {habit.title}
-            </span>
-          ))}
-        </div>
+        {habitsArray.length != 0 ? (
+          <div className={styles.habitCardsContainer}>
+            {habitsArray.map((habit) => (
+              <span
+                key={habit.id}
+                className={`${styles.habitCard} ${isActive(habit)}`}
+                onClick={() => setChosenHabit(habit)}
+              >
+                {habit.title}
+              </span>
+            ))}
+          </div>
+        ) : (
+          <div className={styles.noHabitsContainer}>
+            <h2>No habits to choose from</h2>
+          </div>
+        )}
       </div>
     </div>
   );
