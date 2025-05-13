fa659edcead7bf5983893c461078b5b79b71c8be
George <demina.ivann@gmail.com>
2025-05-13 07:12:04 -0700

    Take in account only current day's habits

--- src/components/dashboard/right-sidebar/HabitsPieChart.tsx
+++ src/components/dashboard/right-sidebar/HabitsPieChart.tsx
@@ -2,26 +2,32 @@ import { PieChart, Pie, Cell } from "recharts";
 import styles from "/src/styles/modules/dashboard/right-sidebar/habits-pie-chart.module.scss";
 import { useHabit } from "../../../contexts/HabitContext";
 import { useLocation } from "react-router-dom";
+import { getWeekday } from "../../../helpers/date/getWeekday";
 
 export default function HabitsPieChart() {
   const { habits: userHabits } = useHabit();
-  const habits = Object.values(userHabits);
+  const allHabits = Object.values(userHabits);
+  const todayHabits = allHabits.filter((h) =>
+    h.days.includes(getWeekday(new Date()))
+  );
   const location = useLocation();
   const getSearchParamDateKey = () => {
     const searchParams = new URLSearchParams(location.search);
     return searchParams.get("date") ?? "00-00-0000";
   };
-  if (habits.length > 0) {
+  if (todayHabits.length > 0) {
     const date = getSearchParamDateKey();
-    const totalCompleted = habits.filter((element) => {
+    const totalCompleted = todayHabits.filter((element) => {
       const history = element.history;
       if (history[date]) {
         const historyRecord = history[date];
         return historyRecord.isCompleted;
       }
     }).length;
-    const totalUncompleted = habits.length - totalCompleted;
-    const completedRate = Math.floor((totalCompleted / habits.length) * 100);
+    const totalUncompleted = todayHabits.length - totalCompleted;
+    const completedRate = Math.floor(
+      (totalCompleted / todayHabits.length) * 100
+    );
 
     const data = [
       { name: "Complete", value: totalCompleted },
